9cd5c56886b69cf7b2921e18fe1a88541d687348
Mads Slotsbo <mads.slotsbo@gmail.com>
2020-04-29 18:10:57 +0200

    Allowing service worker to self update

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,33 @@
 import "./gronia-timereg";
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
+  let newWorker : ServiceWorker | null;
+  navigator.serviceWorker.register('/sw.js').then((registration) => {
+    registration.addEventListener('updatefound', () => {
+      newWorker = registration.installing;
+
+      registration.installing?.addEventListener('statechange', (event) => {
+
+        switch (newWorker?.state) {
+          case 'installed':
+            if (navigator.serviceWorker.controller) {
+              // TODO: Query user for this action
+              setTimeout(() => {
+                console.log("GOGOGO!");
+                newWorker?.postMessage({ action: 'skipWaiting' });
+              }, 5000);
+              console.log('installed');
+            }
+            break;
+        }
+      });
+    });
+
+    let refreshing = false;
+    navigator.serviceWorker.addEventListener('controllerchange', function () {
+      if (refreshing) return;
+      window.location.reload();
+      refreshing = true;
+    });
+  });
 }
--- src/sw/sw.ts
+++ src/sw/sw.ts
@@ -5,4 +5,12 @@ import { precacheAndRoute } from 'workbox-precaching';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+self.addEventListener('message', async event => {
+  switch (event.data.action) {
+    case 'skipWaiting':
+      await self.skipWaiting();
+      break;
+  }
+});
+
 export default null;
