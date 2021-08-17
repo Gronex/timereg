d094d4808827a995157178d76cadcd776503ce4d
Mads Slotsbo <mads.slotsbo@gmail.com>
2021-08-17 21:02:57 +0200

    Removing netlify cli dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,7 @@
-import postcss from 'rollup-plugin-postcss'
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
   plugins: [
-    postcss({
-      extract: true,
-      plugins: []
-    }),
     nodeResolve()
   ]
 }
