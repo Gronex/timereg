cfa199d115463e020e2810adf190642938859d34
Mads Slotsbo <mads.slotsbo@gmail.com>
2021-08-27 18:37:24 +0200

    Summing time only to hours

---
+++ src/Gronia.Timereg.Client/Formatting/TimeSpanFormattingExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gronia.Timereg.Client.Formatting
+{
+    public static class TimeSpanFormattingExtensions
+    {
+        public static string Format(this TimeSpan? timeSpan) => timeSpan?.Format() ?? string.Empty;
+
+        public static string Format(this TimeSpan timeSpan) => $"{(int)timeSpan.TotalHours}:{timeSpan:mm\\:ss}";
+    }
+}
